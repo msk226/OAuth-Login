19a82dabc559fbe7491958e49d32abf03c9774f4
msk226 <kminseok5167@gmail.com>
2025-04-28 22:43:49 +0900

    [FEATURE] 카카오 소셜 로그인 위한 외부 API 호출 관련 코드 FeignClient로 구현

---
+++ src/main/java/study/login/client/KaKaoApiClient.java
@@ -0,0 +1,16 @@
+package study.login.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import study.login.dto.kakao.KaKaoUser;
+
+@FeignClient(name = "kakaoApiClient", url = "https://kapi.kakao.com")
+public interface KaKaoApiClient {
+
+	@GetMapping("/v2/user/me")
+	KaKaoUser getKaKaoUserInfo(
+			@RequestHeader("Authorization") String accessToken,
+			@RequestHeader("Content-Type") String contentType);
+}
---
+++ src/main/java/study/login/client/KaKaoAuthClient.java
@@ -0,0 +1,20 @@
+package study.login.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import study.login.dto.kakao.KaKaoOAuthToken;
+
+@FeignClient(name = "kakaoAuthClient", url = "https://kauth.kakao.com")
+public interface KaKaoAuthClient {
+
+	@PostMapping("/oauth/token")
+	KaKaoOAuthToken.KaKaoOAuthTokenDTO getKaKaoAccessToken(
+			@RequestHeader("Content-Type") String contentType,
+			@RequestParam String grant_type,
+			@RequestParam String redirectUri,
+			@RequestParam String client_id,
+			@RequestParam(defaultValue = "authorization_code") String code);
+}
